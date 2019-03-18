0178067765749463f7083e5fc29fb0cf75825b26
UmeshIghe <umeshighe@gmail.com>
2019-03-18 11:54:29 +0530

    updated code to add assertions

--- scr/src/test/java/com/java/flows/ScannerExample.java
+++ scr/src/test/java/com/java/flows/ScannerExample.java
@@ -1,40 +1,38 @@
 package com.scr.responsiblegold.utils;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
+import org.apache.commons.lang3.StringUtils;
+
+import org.junit.Assert;
+
 public class ScannerExample {
-//	public static void main(String[] args) throws IOException {
-	public static void headerVerify(String response) {
-	
-//		String content = new String(Files.readAllBytes(Paths.get("C:\\\\Users\\\\e070376\\\\eclipse-workspace\\\\TechnicalWriter\\\\src\\\\main\\\\resources\\\\chromeDriver\\\\response.csv")));
-		// Get scanner instance
-	
+
+	public static void headerVerify(String response, String expcsvheaders) {
+
+//		String expHeaders[] = expcsvheaders.split(",");
+
 		String content = response;
 		Scanner scanner = new Scanner(content);
 
-		// Set the delimiter used in file
 		scanner.useDelimiter(",");
 
-		// Get all tokens and store them in some data structure
-		// I am just printing them
 		while (scanner.hasNext()) {
-//			System.out.print(scanner.nextLine());
+
 			String alpha = scanner.nextLine();
 			List<String> result = Arrays.asList(alpha.split("\\s*,\\s*"));
 			for (String col : result) {
-				System.out.println(col);
+				System.out.println(col.replace("\"", StringUtils.EMPTY));
+				if (!expcsvheaders.contains(col.trim().replace("\"", StringUtils.EMPTY))) {
+					Assert.assertTrue("Header is not Present :" + col, false);
+				}
+
 			}
 			break;
 		}
 
-		// Do not forget to close the scanner
 		scanner.close();
 	}
 }
